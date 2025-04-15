7bee90ba0cc84da8ec0d4174b87703e7b54b1b72
Anh Quan Nguyen <forwork.quanna@gmail.com>
2025-04-15 23:30:44 +0700

    init code base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aq2208/goload
+
+go 1.23.5
---
+++ internal/database/migrations/mysql/goload_mysql_script_v1.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE goload;
+
+USE goload;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(100),
+    password_hash VARCHAR(255),         -- nullable if using OAuth
+    auth_provider ENUM('google', 'local') NOT NULL DEFAULT 'local',
+    google_id VARCHAR(100),             -- optional: store Google account ID
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE download_tasks (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    download_type ENUM('http', 'ftp', 'torrent') NOT NULL DEFAULT 'http',
+    url TEXT NOT NULL,
+    metadata JSON,                           -- flexible for file info, headers, etc.
+    status ENUM('queued', 'in_progress', 'completed', 'failed') NOT NULL DEFAULT 'queued',
+    file_path TEXT,                          -- local/S3 path to downloaded file
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_tokens (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    refresh_token VARCHAR(512) NOT NULL,
+    user_agent VARCHAR(255),
+    ip_address VARCHAR(50),
+    expires_at DATETIME NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    UNIQUE(user_id, refresh_token)
+);
